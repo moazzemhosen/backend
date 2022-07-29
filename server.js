255caeed23c48142f435e4886d85f5f670a1557f
moazzemhosen <92745004+moazzemhosen@users.noreply.github.com>
2022-07-29 21:32:11 +0530

    update

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+const socket = require("socket.io");
+const io = socket(server);
+
+const port =  process.env.PORT;
+// used in older vertions
+
+
+
+io.on("connection",(socket)=>{
+    socket.emit("me",socket.id)
+
+    socket.on("disconnect",()=>{
+        socket.broadcast.emit("callEnded")
+    })
+
+    socket.on("callUser",(data)=>{
+        io.to(data.userToCall).emit("callUser",{signal:data.signalData, from:data.from, name:data.name})
+    })
+
+    socket.on("answerCall",(data)=>{
+        io.to(data.to).emit("callAccepted",data.signal)
+
+    })
+})
+server.listen(port, () => console.log("server is listning on port 5000"));
